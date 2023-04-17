7fb3eb7f2d3b86418c93e47760f65766595fc93a
ViktorM <viktor@malmedal.dev>
2023-04-17 11:01:27 +0200

    remove console.log

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 export * from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-console.log({ dbUrl: process.env.DATABASE_URL });
 
 export const prisma =
   globalForPrisma.prisma ||

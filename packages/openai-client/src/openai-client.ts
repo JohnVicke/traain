7f2469241f17faa9bc878ceb3a0934fc3f747cf1
ViktorM <viktor@malmedal.dev>
2023-04-17 14:23:50 +0200

    remove basepath

--- packages/openai-client/src/openai-client.ts
+++ packages/openai-client/src/openai-client.ts
@@ -1,32 +1,46 @@
-import { Configuration, OpenAIApi, type CreateCompletionRequest } from "openai";
+import {
+  Configuration,
+  OpenAIApi,
+  type CreateChatCompletionRequest,
+} from "openai";
 
 export class OpenAiClient {
   private readonly client: OpenAIApi;
 
-  constructor(apiKey: string, basePath = "https://api.openai.com/v1") {
+  constructor(apiKey: string) {
     const configuration = new Configuration({
+      organization: "org-oFy2JhAxjHSuMWEwCkfgR3aQ",
       apiKey,
-      basePath,
     });
     this.client = new OpenAIApi(configuration);
   }
 
   public async complete(
-    options: Pick<CreateCompletionRequest, "prompt" | "model">,
+    options: Pick<CreateChatCompletionRequest, "messages">,
   ) {
-    const response = await this.client.createCompletion({
+    const response = await this.client.createChatCompletion({
       temperature: 0.9,
-      max_tokens: 1000,
+      max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
+      model: "gpt-3.5-turbo",
       ...options,
     });
 
-    if (!response.data.choices[0]?.text) {
+    if (!response.data.choices[0]?.message?.content) {
       throw new Error("No choices returned from OpenAI");
     }
 
-    return response.data.choices[0]?.text;
+    return response.data.choices[0].message.content;
   }
+}
+
+let aiClient: OpenAiClient | null = null;
+
+export function createOpenAiClient(): OpenAiClient {
+  if (!aiClient) {
+    aiClient = new OpenAiClient(process.env.OPENAI_API_KEY as string);
+  }
+  return aiClient;
 }

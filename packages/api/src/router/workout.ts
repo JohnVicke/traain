96e32261954cd53d22baa5a8a52ae021fcc1f685
ViktorM <viktor@malmedal.dev>
2023-04-17 22:44:42 +0200

    update input

--- packages/api/src/router/workout.ts
+++ packages/api/src/router/workout.ts
@@ -3,36 +3,41 @@ import { z } from "zod";
 
 import { createOpenAiClient } from "@traain/openai-client/src/openai-client";
 
+import { equipmentStyleSchema } from "../schemas/equipment-style";
+import { muscleGroupSchema } from "../schemas/muscle-group";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 const aiClient = createOpenAiClient();
 
 export const workoutRouter = createTRPCRouter({
-  create: publicProcedure.mutation(async ({ ctx }) => {
-    try {
-      const response = await aiClient.complete({
-        messages: [
-          {
-            role: "user",
-            content:
-              "1. Chest, triceps, biceps, back, 2. 2 hours, 3. Fully equipped gym",
-          },
-        ],
-      });
-      return response;
-    } catch (error) {
-      console.error("Error creating workout", error);
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-      });
-    }
-  }),
-  byId: publicProcedure
-    .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.post.findFirst({ where: { id: input.id } });
+  create: publicProcedure
+    .input(
+      z.object({
+        muscleGroups: z.array(muscleGroupSchema),
+        minutes: z.number().min(1),
+        equipmentStyle: equipmentStyleSchema,
+      }),
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const hours = Math.floor(input.minutes / 60);
+        const content = `1. ${input.muscleGroups.join(
+          ", ",
+        )}, 2. ${hours}h, 3. ${input.equipmentStyle}`;
+        const response = await aiClient.complete({
+          messages: [
+            {
+              role: "user",
+              content,
+            },
+          ],
+        });
+        return response;
+      } catch (error) {
+        console.error("Error creating workout", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
     }),
-  delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.prisma.post.delete({ where: { id: input } });
-  }),
 });

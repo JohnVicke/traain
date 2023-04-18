fa5f8b65687e94c01d6252ae59980299455b327d
ViktorM <viktor@malmedal.dev>
2023-04-18 15:11:43 +0200

    add clerk and protected route provider

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,30 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Constants from "expo-constants";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { ClerkProvider } from "@clerk/clerk-expo";
 
 import { TRPCProvider } from "~/utils/api";
+import { tokenCache } from "~/utils/token-store";
+import { ProtectedRouteProvider } from "./protected-route";
 
-// This is the main layout of the app
-// It wraps your pages with the providers they need
 const RootLayout = () => {
   return (
-    <TRPCProvider>
-      <SafeAreaProvider>
-        {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
-        */}
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#f472b6",
-            },
-          }}
-        />
-        <StatusBar />
-      </SafeAreaProvider>
-    </TRPCProvider>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={Constants?.expoConfig?.extra?.clerk.publishableKey}
+    >
+      <ProtectedRouteProvider>
+        <TRPCProvider>
+          <SafeAreaProvider>
+            <Stack
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: "#f472b6",
+                },
+              }}
+            />
+            <StatusBar />
+          </SafeAreaProvider>
+        </TRPCProvider>
+      </ProtectedRouteProvider>
+    </ClerkProvider>
   );
 };
 

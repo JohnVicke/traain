f02b10cc92421db97fa30ef737f35d1bf5e8444a
ViktorM <viktor@malmedal.dev>
2023-04-18 20:26:49 +0200

    show sign in and sign out

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link, Stack } from "expo-router";
+import { SignedIn, SignedOut } from "@clerk/clerk-expo";
 
 import { SignOutButton } from "~/components/sign-out-button";
 
@@ -13,18 +14,21 @@ export default function Root() {
         <Text className="mx-auto pb-2 text-5xl font-bold text-white">
           tra<Text className="text-pink-400">ai</Text>n
         </Text>
-
-        <Link href="/sign-up" asChild>
-          <Pressable>
-            {() => <Text className="text-white">Sign up</Text>}
-          </Pressable>
-        </Link>
-        <Link href="/workout" asChild>
-          <Pressable>
-            {() => <Text className="text-white">To workout screen (auth)</Text>}
-          </Pressable>
-        </Link>
-        <SignOutButton />
+        <SignedIn>
+          <Link href="/workout/generate" asChild>
+            <Pressable>
+              {() => <Text className="text-white">Generate workout</Text>}
+            </Pressable>
+          </Link>
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href="/sign-in" asChild>
+            <Pressable>
+              {() => <Text className="text-white">Sign in</Text>}
+            </Pressable>
+          </Link>
+        </SignedOut>
       </View>
     </SafeAreaView>
   );

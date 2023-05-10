fb63c442f7a1a6f288aa24fb647a2a3a4ca5126f
ViktorM <viktor@malmedal.dev>
2023-05-10 05:01:38 +0200

    wip swipeable action

--- apps/expo/src/modules/workout/workout-list.tsx
+++ apps/expo/src/modules/workout/workout-list.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, type Animated } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
 import { FlashList } from "@shopify/flash-list";
-import { MoreVertical, PlusSquare } from "lucide-react-native";
-import { MotiView } from "moti";
+import { MoreVertical, PlusSquare, Trash } from "lucide-react-native";
+import { AnimatePresence, MotiView } from "moti";
 import { useForm } from "react-hook-form";
 
 import { type RouterOutputs } from "@traain/api";
@@ -132,59 +133,80 @@ function SetRow(props: SetRowProps) {
   });
 
   return (
-    <TouchableOpacity
-      onLongPress={() => handleOpenModal()}
-      className="mb-2 flex flex-row items-center"
+    <Swipeable
+      childrenContainerStyle={{ flex: 1 }}
+      renderRightActions={(progress, dragX) => (
+        <RightActions progress={progress} dragX={dragX} />
+      )}
     >
-      <View className="relative h-6 w-6 items-center justify-center rounded-full bg-slate-500/50 p-2">
-        <Text className="absolute text-xs text-slate-200">
-          {props.index + 1}
-        </Text>
-      </View>
-      <View className="ml-4 flex min-w-[50px]">
-        <Text className="text-[10px] text-slate-400">Weight</Text>
-        <TextInputField
-          onBlur={() => void updateSet()}
-          variant="ghost"
-          control={control}
-          name="weight"
-          placeholder="0"
-          className="p-1 font-bold"
-          keyboardType="number-pad"
+      <View className="mb-2 flex flex-row items-center">
+        <View className="relative h-6 w-6 items-center justify-center rounded-full bg-slate-500/50 p-2">
+          <Text className="absolute text-xs text-slate-200">
+            {props.index + 1}
+          </Text>
+        </View>
+        <View className="ml-4 flex min-w-[50px]">
+          <Text className="text-[10px] text-slate-400">Weight</Text>
+          <TextInputField
+            onBlur={() => void updateSet()}
+            variant="ghost"
+            control={control}
+            name="weight"
+            placeholder="0"
+            className="p-1 font-bold"
+            keyboardType="number-pad"
+          />
+        </View>
+        <View className="ml-2 flex min-w-[50px]">
+          <Text className="text-[10px] text-slate-400">Reps</Text>
+          <TextInputField
+            onBlur={() => void updateSet()}
+            variant="ghost"
+            control={control}
+            name="reps"
+            placeholder="0"
+            className="p-1 font-bold"
+            keyboardType="number-pad"
+          />
+        </View>
+        <View className="ml-2 flex min-w-[50px]">
+          <Text className="text-[10px] text-slate-400">Notes</Text>
+          <TextInputField
+            onBlur={() => void updateSet()}
+            variant="ghost"
+            control={control}
+            name="notes"
+            placeholder={props.set.notes ?? "Add a note..."}
+            className="rounded p-1"
+            keyboardType="default"
+          />
+        </View>
+        <TouchableOpacity className="ml-auto" onPress={handleOpenModal}>
+          <MoreVertical color="white" width={18} height={18} />
+        </TouchableOpacity>
+        <EditSetHalfModal
+          id={props.set.id}
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
         />
       </View>
-      <View className="ml-2 flex min-w-[50px]">
-        <Text className="text-[10px] text-slate-400">Reps</Text>
-        <TextInputField
-          onBlur={() => void updateSet()}
-          variant="ghost"
-          control={control}
-          name="reps"
-          placeholder="0"
-          className="p-1 font-bold"
-          keyboardType="number-pad"
-        />
-      </View>
-      <View className="ml-2 flex min-w-[50px]">
-        <Text className="text-[10px] text-slate-400">Notes</Text>
-        <TextInputField
-          onBlur={() => void updateSet()}
-          variant="ghost"
-          control={control}
-          name="notes"
-          placeholder={props.set.notes ?? "Add a note..."}
-          className="rounded p-1"
-          keyboardType="default"
-        />
-      </View>
-      <TouchableOpacity className="ml-auto" onPress={handleOpenModal}>
-        <MoreVertical color="white" width={18} height={18} />
-      </TouchableOpacity>
-      <EditSetHalfModal
-        id={props.set.id}
-        open={modalOpen}
-        onClose={() => setModalOpen(false)}
-      />
-    </TouchableOpacity>
+    </Swipeable>
   );
 }
+
+const RightActions = (props: {
+  progress: Animated.AnimatedInterpolation<string | number>;
+  dragX: Animated.AnimatedInterpolation<string | number>;
+}) => {
+  const { progress, dragX } = props;
+  return (
+    <MotiView
+      from={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="flex flex-row items-center space-x-2 bg-red-400"
+    >
+      <Trash className="text-slate-800" />
+      <Text>Delete</Text>
+    </MotiView>
+  );
+};
